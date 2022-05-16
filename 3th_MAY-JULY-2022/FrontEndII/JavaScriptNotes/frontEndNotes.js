8b477fef1c018bd244453bc3b7f8aa671bc948b3
Nathany <naty_om_3@hotmail.com>
2022-05-16 19:19:16 -0300

    Adding some code to practice

--- 3th_MAY-JULY-2022/FrontEndII/JavaScriptNotes/frontEndNotes.js
+++ 3th_MAY-JULY-2022/FrontEndII/JavaScriptNotes/frontEndNotes.js
@@ -1,1 +1,13 @@
-alert("Este é um alerta."); 
+/*LESSON 1 - Desenvolver um script que permita somar um array de números consecutivos, de forma que se some o primeiro número com o segundo e o imprima através do console. 
+Depois, temos que pegar este resultado e somar o terceiro número, e assim por diante, até termos terminado de percorrer todo o array.
+*/
+
+
+
+const array = [1, 2, 4, 8]; 
+let sum = 0; 
+for (let i = 0; i < array. length; i++) 
+{ sum += array[i]; } 
+console. log(sum);
+
+
