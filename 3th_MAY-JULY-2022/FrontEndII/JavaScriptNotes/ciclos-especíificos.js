ba2c77ab2a95d99a3eb6acd6fd543f180ac24973
Nathany <naty_om_3@hotmail.com>
2022-05-24 17:20:05 -0300

    Adding some practice we did in class

---
+++ 3th_MAY-JULY-2022/FrontEndII/JavaScriptNotes/ciclos-especíificos.js
@@ -0,0 +1,15 @@
+/*let carro = {
+    nome: "fox",
+    ano: "2021"
+}
+
+for(let prop in carro){
+    console.log(carro[prop])
+}*/
+
+
+let series = ["Friends", "Game of thrones", "Breaking Bad"];
+
+for(let valor of series){
+    console.log(valor)
+}
--- 3th_MAY-JULY-2022/FrontEndII/JavaScriptNotes/frontEndNotes.js
+++ 3th_MAY-JULY-2022/FrontEndII/JavaScriptNotes/frontEndNotes.js
@@ -17,3 +17,6 @@ for(let i = 0; i < array.length; i++)
 {sum += array[i]
 console.log(sum);
 }
+
+
+
